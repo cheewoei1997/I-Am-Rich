3b55e2d8b5bba2ed3812a1514744477b2b0a8431
Chee Woei Phuah <cwphuah@Wei-Mings-MacBook-Pro-2.local>
2019-04-10 11:48:22 +0800

    First iOS app

---
+++ I Am Rich/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  I Am Rich
+//
+//  Created by Chee Woei Phuah on 4/9/19.
+//  Copyright © 2019 Chee Woei Phuah. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+
+}
+
